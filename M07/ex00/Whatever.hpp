68ea4a7cfdb4fcf39aa03bdc9c29adbc5c67bf5c
Ahmed Yakoubi <ahmedyakoubi913@gmail.com>
2023-11-24 11:39:04 +0100

    fixed ex00 M07

--- M07/ex00/Whatever.hpp
+++ M07/ex00/Whatever.hpp
@@ -6,7 +6,7 @@
 /*   By: ayakoubi <ayakoubi@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/23 11:53:57 by ayakoubi          #+#    #+#             */
-/*   Updated: 2023/11/23 12:29:44 by ayakoubi         ###   ########.fr       */
+/*   Updated: 2023/11/24 11:38:34 by ayakoubi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,15 +15,32 @@
 
 #include <iostream>
 
+// __ Swap Function ____________________________________________________________
+// =============================================================================
 template <class T>
-
-class Whatever
+void	swap(T &first, T &second)
 {
-	public:
-		Whatever();
-		void swap(T first, T second);
-		T min(T first, T second);
-		T max(T first, T second);
-};
+	T tmp;
+
+	tmp = first;
+	first = second;
+	second = tmp;
+}
+
+// __ Min Function _____________________________________________________________
+// =============================================================================
+template <class T>
+T	min(T first, T second)
+{
+	return (first < second ? first : second);
+}
+
+// __ Max Function _____________________________________________________________
+// =============================================================================
+template <class T>
+T	max(T first, T second)
+{
+	return (first > second ? first : second);
+}
 
 #endif
--- M07/ex00/main.cpp
+++ M07/ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: ayakoubi <ayakoubi@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/23 12:12:47 by ayakoubi          #+#    #+#             */
-/*   Updated: 2023/11/23 12:31:43 by ayakoubi         ###   ########.fr       */
+/*   Updated: 2023/11/24 11:38:35 by ayakoubi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,13 +14,20 @@
 
 int main()
 {
-	Whatever<int> w;
-	int a = 16;
-	int b = 4;
+	int a = 2;
+	int b = 3;
 
-	w.swap(a, b);
-	std::cout << "a = " << a << std::endl;
-	std::cout << "b = " << b << std::endl;
+	::swap(a, b);
+	std::cout << "a = " << a << ", b = " << b << std::endl;
+	std::cout << "min( a, b ) = " << ::min( a, b ) << std::endl;
+	std::cout << "max( a, b ) = " << ::max( a, b ) << std::endl;
+
+	std::string c = "chaine1";
+	std::string d = "chaine2";
+	::swap(c, d);
+	std::cout << "c = " << c << ", d = " << d << std::endl;
+	std::cout << "min( c, d ) = " << ::min( c, d ) << std::endl;
+	std::cout << "max( c, d ) = " << ::max( c, d ) << std::endl;
 	return (0);
 }
 
