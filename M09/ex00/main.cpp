4e639951ab69ba15f303e386ebc7dde7906ff5c3
Ahmed Yakoubi <ahmedyakoubi913@gmail.com>
2023-12-16 20:30:21 +0100

    dsgdsf

--- M09/ex00/main.cpp
+++ M09/ex00/main.cpp
@@ -6,25 +6,44 @@
 /*   By: ayakoubi <ayakoubi@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/13 11:44:04 by ayakoubi          #+#    #+#             */
-/*   Updated: 2023/12/15 15:24:36 by ayakoubi         ###   ########.fr       */
+/*   Updated: 2023/12/16 20:28:43 by ayakoubi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "BitcoinExchange.hpp"
 
-bool	parsingLine(std::string line)
+void	split_line(std::string line, int i, int j, char **str)
 {
-	char *str[3];
+	int size = static_cast<int>(line.length());
+	int k;
+	int c = i;
+	int len;
+	c++;
+	while (++i < size && line[i] != '-');
+	len = i - c;
+	str[j] = new char[len];
+	k = -1;
+	while (++k < len)
+		str[j][k] = line[c++];
+	if (i < size)
+		split_line(line, i, ++j, str);
+}
+
+
+
+void	parsingLine(std::string line)
+{
+	char *str[4];
 
 	int i;
 	int j;
 	i = -1;
-	while (++i < line.lenght() && line[i] == '-');
-	str[j] = new char[i];
-	if (str
-
-
-		
+	j = 0;
+	split_line(line, i, j, str);
+	str[3] = NULL;
+	i = -1;
+	while (str[++i])
+		std::cout << str[i] << std::endl;
 }
 
 
